21e51f2f160238955b15ec37ab6c343f9108a0a4
kayky233 <1787963736@qq.com>
2026-01-01 21:57:12 +0800

    Sure! Pl

---
+++ demo_c_project/include/calculator.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+int divide(int a, int b, int* error);
+
+#ifdef __cplusplus
+}
+#endif
+
